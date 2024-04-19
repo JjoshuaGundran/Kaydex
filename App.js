93a392cf5665462271ec6f5c745bb705e5e84502
JjoshuaGundran <jjgundran@gmail.com>
2024-04-19 17:51:02 -0500

    added packages for navigation and created the buttons

--- App.js
+++ App.js
@@ -1,20 +1,90 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TouchableOpacity,
+  StatusBar
+} from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+function HomeScreen({ navigation }) {
+  const lists = [1,2,3,4,5,6]
+
+  return (
+    <View style={styles.container}>
+      {lists.map((item) => (
+        <TouchableOpacity key={item} style={styles.optionContainer} onPress={()=>navigation.navigate('About', {idolId: item})}>
+          <Text style={styles.baseText}>One Thing</Text>
+        </TouchableOpacity>
+      ))}
+    </View>
+  )
+}
+
+function AboutScreen({ route }) {
+  const idolId = JSON.stringify(route.params.idolId)
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.baseText}>{idolId}</Text>
+    </View>
+  )
+}
+
+const Stack = createNativeStackNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name='Home'
+          component={HomeScreen}
+          options={{
+            title: 'Kaydex',
+            headerStyle: {
+              backgroundColor: 'red',
+            },
+            headerTintColor: '#fff'
+          }}
+        />
+        <Stack.Screen
+          name='About'
+          component={AboutScreen}
+          options={{
+            title: 'About',
+            headerStyle: {
+              backgroundColor: 'red',
+            },
+            headerTintColor: '#fff'
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 10,
+    maxWidth: '100%',
+    gap: 5,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-evenly',
+    backgroundColor: '#212121',
   },
+  baseText: {
+    color: 'white'
+  },
+  optionContainer: {
+    width: 200,
+    height: 200,
+    backgroundColor: 'red',
+    borderRadius: 5,
+  }
 });
